b41ebfc68d1599f9aade406455abaa4933fa478f
brodyamcfarland <brodyamcfarland@gmail.com>
2023-01-11 16:33:27 -0800

    Fixed Some Folder Structure and Reworking Smart Contract

--- src/contracts/CoinCampV2.sol
+++ src/contracts/CoinCampV2.sol
@@ -1,129 +1,77 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
-interface IERC20 {
-    function transfer(address, uint) external returns (bool);
-
-    function transferFrom(
-        address,
-        address,
-        uint
-    ) external returns (bool);
-}
-
-contract CrowdFund {
+contract CrowdFunding {
     struct Campaign {
-        address creator;
-        uint goal;
-        uint pledged;
-        uint startAt;
-        uint endAt;
-        bool claimed;
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        address[] donators;
+        uint256[] donations;
+        bool active;
     }
 
-    IERC20 public immutable token;
-    uint public count;
-    uint public maxDuration;
-    mapping(uint => Campaign) public campaigns;
-    mapping(uint => mapping(address => uint)) public pledgedAmount;
+    mapping(uint256 => Campaign) public campaigns;
 
-    event Launch(
-        uint id,
-        address indexed creator,
-        uint goal,
-        uint32 startAt,
-        uint32 endAt
-    );
-    event Cancel(uint id);
-    event Pledge(uint indexed id, address indexed caller, uint amount);
-    event Unpledge(uint indexed id, address indexed caller, uint amount);
-    event Claim(uint id);
-    event Refund(uint id, address indexed caller, uint amount);
+    uint16 public numberOfCampaigns = 0;
 
-    constructor(address _token, uint _maxDuration) {
-        token = IERC20(_token);
-        maxDuration = _maxDuration;
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.active = true;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
     }
 
-    function launch(uint _goal, uint32 _startAt, uint32 _endAt) external {
-        require(_startAt >= block.timestamp,"Start time is less than current Block Timestamp");
-        require(_endAt > _startAt,"End time is less than Start time");
-        require(_endAt <= block.timestamp + maxDuration, "End time exceeds the maximum Duration");
-
-        count += 1;
-        campaigns[count] = Campaign({
-            creator: msg.sender,
-            goal: _goal,
-            pledged: 0,
-            startAt: _startAt,
-            endAt: _endAt,
-            claimed: false
-        });
-
-        emit Launch(count,msg.sender,_goal,_startAt,_endAt);
-    }
-
-    function cancel(uint _id) external {
-        Campaign memory campaign = campaigns[_id];
-        require(campaign.creator == msg.sender, "You did not create this Campaign");
-        require(block.timestamp < campaign.startAt, "Campaign has already started");
-
-        delete campaigns[_id];
-        emit Cancel(_id);
-    }
-
-    function pledge(uint _id, uint _amount) external {
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
         Campaign storage campaign = campaigns[_id];
-        require(block.timestamp >= campaign.startAt, "Campaign has not Started yet");
-        require(block.timestamp <= campaign.endAt, "Campaign has already ended");
-        campaign.pledged += _amount;
-        pledgedAmount[_id][msg.sender] += _amount;
-        token.transferFrom(msg.sender, address(this), _amount);
+        require(campaign.active == true, "The Donation Period has ended");
+        require(block.timestamp < campaign.deadline, "The Deadline has passed.");
 
-        emit Pledge(_id, msg.sender, _amount);
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+        if(campaign.amountCollected > campaign.target) {
+            campaign.active = false;
+        }
     }
 
-    function unPledge(uint _id,uint _amount) external {
-        Campaign storage campaign = campaigns[_id];
-        require(block.timestamp >= campaign.startAt, "Campaign has not Started yet");
-        require(block.timestamp <= campaign.endAt, "Campaign has already ended");
-        require(pledgedAmount[_id][msg.sender] >= _amount,"You do not have enough tokens Pledged to withraw");
-
-        campaign.pledged -= _amount;
-        pledgedAmount[_id][msg.sender] -= _amount;
-        token.transfer(msg.sender, _amount);
-
-        emit Unpledge(_id, msg.sender, _amount);
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function claim(uint _id) external {
-        Campaign storage campaign = campaigns[_id];
-        require(campaign.creator == msg.sender, "You did not create this Campaign");
-        require(block.timestamp > campaign.endAt, "Campaign has not ended");
-        require(campaign.pledged >= campaign.goal, "Campaign did not succed");
-        require(!campaign.claimed, "claimed");
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
-        campaign.claimed = true;
-        token.transfer(campaign.creator, campaign.pledged);
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
 
-        emit Claim(_id);
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
     }
 
-    function refund(uint _id) external {
-        Campaign memory campaign = campaigns[_id];
-        require(block.timestamp > campaign.endAt, "not ended");
-        require(campaign.pledged < campaign.goal, "You cannot Withdraw, Campaign has succeeded");
-
-        uint bal = pledgedAmount[_id][msg.sender];
-        pledgedAmount[_id][msg.sender] = 0;
-        token.transfer(msg.sender, bal);
-
-        emit Refund(_id, msg.sender, bal);
+    function getTime() public view returns (uint) {
+        return uint(block.timestamp);
     }
-
-    function getTimestamp() public view returns (uint) {
-        return block.timestamp;
-    }
-
-
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import network from "../../utils/network";
+import network from "../utils/network";
 import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
